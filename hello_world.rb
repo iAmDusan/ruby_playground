bdcdba03457915ddf58a047828a82616902ee5cc
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2024-06-11 13:38:01 -0400

    first commmit

---
+++ hello_world.rb
@@ -0,0 +1,21 @@
+# hello_world.rb
+
+# String
+greeting = "Hello, World!"
+puts greeting
+
+# Integer
+age = 30
+puts "I am #{age} years old."
+
+# Float
+height = 5.9
+puts "I am #{height} feet tall."
+
+# Boolean
+is_happy = true
+puts "Am I happy? #{is_happy}"
+
+# Array
+fruits = ["apple", "banana", "cherry"]
+puts "I like #{fruits.join(', ')}."
